cd475818df917794ef937049659d15698e2ff221
ronaknavadiya <ronaknavadiya123@gmail.com>
2022-09-25 21:18:35 -0400

    Backend register and login api done

--- backend/server.js
+++ backend/server.js
@@ -7,16 +7,20 @@ import bodyParser from "body-parser";
 import userRoute from "./routes/userRoutes";
 
 const app = express();
+require("dotenv").config();
 app.use(bodyParser.json());
 app.use(cors());
+const port = process.env.PORT | 5000;
 
 mongoose.connect(
-  "mongodb://localhost:27017/",
+  // "mongodb://localhost:27017/",
+
+  process.env.mongoURI,
   {
-    dbName: "authentication-app",
+    // dbName: "authentication-app",
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true,
+    // useCreateIndex: true,
   },
   (err) => (err ? console.log(err) : console.log("Connected to Database"))
 );
@@ -36,6 +40,6 @@ app.post("/api/login", async (req, res) => {
   }
 });
 
-app.listen(4000, () => {
+app.listen(port, () => {
   console.log("Server started");
 });
